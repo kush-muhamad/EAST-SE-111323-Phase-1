a5aebc477566e4754c7c9b6290bb65b026d2443f
kush Muhamad <kushmohammed98@yahoo.com>
2024-03-01 01:47:39 +0300

    Bauji03_solution

--- src/index.js
+++ src/index.js
@@ -55,3 +55,98 @@ const otherFoods = [
 ]
 
 // write your code here
+
+//getElemntbyId - finds the element by its id
+const restaurantMenu = document.getElementById("restaurant-menu")
+
+
+// .querySelector - finds the first element by either its className or id
+const detailImage = document.querySelector(".detail-image")
+
+//get querySelectorAll- finds  a group of elements
+const divElements = document.querySelectorAll("div")
+//console.log(divElements)
+
+// for(let index = 0; index < divElements.length; index++){
+//     console.log(divElements[index])
+
+// }
+
+//getElementbyClassName -gets element by className
+// const nameElements = document.getElementsByClassName("name")
+//  console.log(nameElements[index])
+
+// for(index = 0; index < nameElements.length; index++){
+//     console.log(nameElements[index])
+// }
+
+// nameElements.forEach((name)=>{
+//     console.log(name)
+
+// })
+
+//forloop vs .forEach()
+//forloop
+
+// for(let i = 0; i < burgers.length; i++){
+//     console.log(burgers[i])
+// }
+
+//.forEach()
+// divElements.forEach((div)=>{
+//     console.log(div)
+
+// })
+
+//  burgers.forEach((burger)=>{
+//    console.log(burger.description)
+
+//  })
+//  removing an element from the dom
+// const headerElement = document.querySelector("header")
+// console.log(headerElement)
+// headerElement.remove()
+
+
+//deliverable 1
+
+for(let burger = 0; burger < burgers.length; burger++){
+    const imgElement = document.createElement('img')
+    imgElement.className = 'burger'
+    imgElement.src = burgers[burger].image
+    restaurantMenu.appendChild(imgElement)
+
+}
+// deliverable 2
+
+otherFoods.forEach((food) => {
+    const imgElement = document.createElement('img')
+    imgElement.src = food.image
+    restaurantMenu.appendChild(imgElement)
+})
+
+//deliverable 3 
+const detailImg = document.getElementsByClassName('detail-image')
+detailImage.src = burgers[0].image
+
+//deliverable 4
+const nameElement = document.querySelector(".name")
+nameElement.textContent = burgers[0].name
+
+//deliverable 5
+const discriptionElement = document.querySelector("#description-display")
+discriptionElement.textContent = burgers[0].description
+
+//deliverable 6
+const restaurantMenuImages = document.querySelectorAll("div, #restaurant-menu , img")
+
+restaurantMenuImages.forEach((image)=>{
+    image.style = "border-style: solid; border-color: orange; border-width: 3px"
+})
+
+//deliverable 7
+const BurgerImage = document.getElementsByClassName('burger')
+
+for(let burger = 0; burger < BurgerImage.length; burger++ ){
+    BurgerImage[burger].style.borderColor = "blue"
+}
